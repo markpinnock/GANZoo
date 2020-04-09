f2434eb7d55381c202aab982a238937d19286b38
Mark <mark.pinnock.18@ucl.ac.uk>
2020-04-09 16:54:48 +0100

    Add training functions and losses, and data loaders

---
+++ utils/DataLoaders.py
@@ -0,0 +1,48 @@
+import numpy as np
+import os
+import tensorflow as tf
+
+
+def imgLoader(file_path, img_list):
+    np.random.shuffle(img_list)
+    N = len(img_list)
+    i = 0
+
+    while i < N:
+        img = np.load(file_path + img_list[i])
+        img = (img - img.min()) / (img.max() - img.min()) * 2 - 1
+        i += 1
+        yield img[::1, ::1, tf.newaxis]
+
+
+def imgPartition(file_path, partition_file):
+    train_list = []
+    val_list = []
+    test_list = []
+
+    with open(file_path + partition_file, 'r') as f:
+        for line in f:
+            if line.split(' ')[1] == '0\n':
+                train_list.append(line.split(' ')[0])
+            elif line.split(' ')[1] == '1\n':
+                val_list.append(line.split(' ')[0])
+            elif line.split(' ')[1] == '2\n':
+                test_list.append(line.split(' ')[0])
+            else:
+                raise ValueError("Incorrect train/val/test index")
+
+    return train_list, val_list, test_list
+
+
+if __name__ == "__main__":
+
+    FILE_PATH = "C:/Users/roybo/OneDrive - University College London/PhD/PhD_Prog/009_GAN_CT/Train/"
+    imgs_list = os.listdir(FILE_PATH)
+    MB_SIZE = 4
+
+    train_ds = tf.data.Dataset.from_generator(
+        imgLoader, args=[FILE_PATH, imgs_list], output_types=tf.float32)
+    
+    for img in train_ds.batch(MB_SIZE):
+        print(img.numpy().min(), img.numpy().max())
+        # pass
---
+++ utils/TrainFuncs.py
@@ -0,0 +1,55 @@
+import tensorflow as tf
+import tensorflow.keras as keras
+
+
+@tf.function
+def genLossDC(fake):
+    BCE = keras.losses.BinaryCrossentropy(from_logits=True)
+    return BCE(tf.ones_like(fake), fake)
+
+
+@tf.function
+def discLossDC(real, fake):
+    BCE = keras.losses.BinaryCrossentropy(from_logits=True)
+    real_loss = BCE(tf.ones_like(real), real)
+    fake_loss = BCE(tf.zeros_like(fake), fake)
+    return real_loss + fake_loss
+
+
+@tf.function
+def genLossLS(fake):
+    return 0.5 * tf.reduce_mean(tf.pow(fake - 1, 2))
+
+
+@tf.function
+def discLossLS(real, fake):
+    return 0.5 * tf.reduce_mean(tf.pow(real - 1, 2)) + 0.5 * tf.reduce_mean(tf.pow(fake, 2))
+
+
+@tf.function
+def trainStep(
+    imgs, Generator, Discriminator, GenOptimiser, DiscOptimiser,\
+        mb_size, noise_dim, genMetric, discMetric1, discMetric2, discAcc1, discAcc2):
+
+    noise = tf.random.uniform((mb_size, noise_dim), -1, 1)
+
+    with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+        gen_img = Generator(noise, training=True)
+        real_pred = Discriminator(imgs + tf.random.normal(imgs.shape.as_list(), 0, 1), training=True)
+        fake_pred = Discriminator(gen_img + tf.random.normal(gen_img.shape.as_list(), 0, 1), training=True)
+        
+        gen_losses = genLossDC(fake_pred)
+        disc_losses = discLossDC(real_pred, fake_pred)
+
+    gen_gradients = gen_tape.gradient(gen_losses, Generator.trainable_variables)
+    disc_gradients = disc_tape.gradient(disc_losses, Discriminator.trainable_variables)
+    GenOptimiser.apply_gradients(zip(gen_gradients, Generator.trainable_variables))
+    DiscOptimiser.apply_gradients(zip(disc_gradients, Discriminator.trainable_variables))
+
+    genMetric.update_state(tf.ones_like(fake_pred), fake_pred)
+    discMetric1.update_state(tf.ones_like(real_pred), real_pred)
+    discMetric2.update_state(tf.zeros_like(fake_pred), fake_pred)
+    discAcc1.update_state(tf.ones_like(real_pred), real_pred)
+    discAcc2.update_state(tf.zeros_like(fake_pred), fake_pred)
+
+    return gen_gradients, disc_gradients
