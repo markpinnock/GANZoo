a3eecefe5f5512a9c03a0a0ead471b1640fdc132
Mark <mark.pinnock.18@ucl.ac.uk>
2020-04-09 17:58:44 +0100

    Updated training functions and losses

--- utils/TrainFuncs.py
+++ utils/TrainFuncs.py
@@ -27,19 +27,36 @@ def discLossLS(real, fake):
 
 
 @tf.function
-def trainStep(
-    imgs, Generator, Discriminator, GenOptimiser, DiscOptimiser,\
-        mb_size, noise_dim, genMetric, discMetric1, discMetric2, discAcc1, discAcc2):
+def P2PgenLossCalc(fake, lambd, gen_img, hi_img):
+    bce = keras.losses.BinaryCrossentropy(from_logits=True)
+    adv_loss = bce(tf.ones_like(fake), fake)
+    L1_loss = tf.reduce_mean(tf.abs(gen_img - hi_img))
 
-    noise = tf.random.uniform((mb_size, noise_dim), -1, 1)
+    return adv_loss + (lambd * L1_loss)
+
+
+@tf.function
+def P2PdiscLossCalc(real, fake):
+    bce = keras.losses.BinaryCrossentropy(from_logits=True)
+    real_loss = bce(tf.ones_like(real) * 0.9, real)
+    fake_loss = bce(tf.zeros_like(fake), fake)
+    total_loss = real_loss + fake_loss
+
+    return 0.5 * total_loss
+
+
+@tf.function
+def trainStep(
+    ACE_img, NCE_img, Generator, Discriminator, GenOptimiser, DiscOptimiser,\
+        lambd, genMetric, discMetric1, discMetric2):
 
     with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
-        gen_img = Generator(noise, training=True)
-        real_pred = Discriminator(imgs + tf.random.normal(imgs.shape.as_list(), 0, 1), training=True)
-        fake_pred = Discriminator(gen_img + tf.random.normal(gen_img.shape.as_list(), 0, 1), training=True)
+        gen_ACE = Generator(NCE_img, training=True)
+        real_pred = Discriminator([NCE_img, ACE_img], training=True)
+        fake_pred = Discriminator([gen_ACE, ACE_img], training=True)
         
-        gen_losses = genLossDC(fake_pred)
-        disc_losses = discLossDC(real_pred, fake_pred)
+        gen_losses = P2PgenLossCalc(fake_pred, lambd, gen_ACE, ACE_img)
+        disc_losses = P2PdiscLossCalc(real_pred, fake_pred)
 
     gen_gradients = gen_tape.gradient(gen_losses, Generator.trainable_variables)
     disc_gradients = disc_tape.gradient(disc_losses, Discriminator.trainable_variables)
@@ -47,9 +64,8 @@ def trainStep(
     DiscOptimiser.apply_gradients(zip(disc_gradients, Discriminator.trainable_variables))
 
     genMetric.update_state(tf.ones_like(fake_pred), fake_pred)
+    gen_MAE = tf.reduce_mean(tf.abs(gen_ACE - ACE_img))
     discMetric1.update_state(tf.ones_like(real_pred), real_pred)
     discMetric2.update_state(tf.zeros_like(fake_pred), fake_pred)
-    discAcc1.update_state(tf.ones_like(real_pred), real_pred)
-    discAcc2.update_state(tf.zeros_like(fake_pred), fake_pred)
 
-    return gen_gradients, disc_gradients
+    return gen_MAE, gen_gradients, disc_gradients
